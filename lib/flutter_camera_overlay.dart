2b5a2a904ff1343d4c53257a2ac25f7f89902f8a
Esteban <esteban@houlak.com>
2023-05-23 09:37:08 -0300

    added

--- lib/flutter_camera_overlay.dart
+++ lib/flutter_camera_overlay.dart
@@ -79,7 +79,7 @@ class _FlutterCameraOverlayState extends State<CameraOverlay> {
       alignment: Alignment.bottomCenter,
       fit: StackFit.expand,
       children: [
-        SizedBox(height: 500, child: CameraPreview(controller)),
+        CameraPreview(controller),
         OverlayShape(widget.model),
         if (widget.label != null || widget.info != null)
           Align(
